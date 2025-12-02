04d736d0eaca1e504cba2ccd94d5be7c7f6e023b
sebastiancielma <cielmasebastian@gmail.com>
2025-12-02 20:23:07 +0100

    fix: add pydanticv2 config structure

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,22 +1,23 @@
 import os
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Traffic AI"
     API_URL: str = "http://localhost:8000"
     MODEL_PATH: str = "yolov8n.pt"
+
     UPLOAD_DIR: str = os.path.join("data", "uploads")
     RESULTS_DIR: str = os.path.join("data", "results")
     API_V1_STR: str = "/api/v1"
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env", extra="ignore", case_sensitive=True
+    )
 
 
 settings = Settings()
-
 
 os.makedirs(settings.UPLOAD_DIR, exist_ok=True)
 os.makedirs(settings.RESULTS_DIR, exist_ok=True)
