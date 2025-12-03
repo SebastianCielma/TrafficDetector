a0623e3c60576c251346de4533c2cac93b5dde6a
sebastiancielma <cielmasebastian@gmail.com>
2025-12-04 00:20:53 +0100

    feat: add dependency injection deps.py

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,8 +1,14 @@
-from functools import lru_cache
+from fastapi import Depends
+from sqlmodel.ext.asyncio.session import AsyncSession
 
-from backend.app.services.yolo import YoloService
+from backend.app.core.db import get_session
+from backend.app.services.file import FileService
+from backend.app.services.task import TaskService
 
 
-@lru_cache()
-def get_yolo_service() -> YoloService:
-    return YoloService()
+async def get_task_service(session: AsyncSession = Depends(get_session)) -> TaskService:
+    return TaskService(session)
+
+
+def get_file_service() -> FileService:
+    return FileService()
