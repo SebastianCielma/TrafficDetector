c83b6446c7e6e134a3b3c14607f1a91af8f44cf5
sebastiancielma <cielmasebastian@gmail.com>
2025-12-04 09:48:49 +0100

    refactor: refactor main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,33 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from backend.app.api.v1.router import router
 from backend.app.core.config import settings
+from backend.app.core.db import init_db
 
-app = FastAPI(title=settings.PROJECT_NAME)
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    print(" Initializing Database connection...")
+    try:
+        await init_db()
+        print("Database tables created successfully.")
+    except Exception as e:
+        print(f"Database connection failed: {e}")
+
+    yield
+
+    print("Shutting down application...")
+
+
+app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
 
 app.include_router(router, prefix=settings.API_V1_STR)
-
 app.mount("/results", StaticFiles(directory=settings.RESULTS_DIR), name="results")
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("backend.app.main:app", host="0.0.0.0", port=8000, reload=True)
